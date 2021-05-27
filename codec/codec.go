587d7f42b36604a8551761a03477f283f5def0e1
Takelau <ds248a@gmail.com>
2021-05-27 15:11:47 +0300

    nrpc

--- codec/codec.go
+++ codec/codec.go
@@ -4,34 +4,29 @@ import (
 	"encoding/json"
 )
 
-// DefaultCodec is the default codec used by arpc
+// DefaultCodec is the default codec used by nrpc.
 var DefaultCodec Codec = &JSONCodec{}
 
-// Codec is the interface that wraps the arpc Message data encoding method.
-//
-// Marshal returns the JSON encoding of v
-//
-// Unmarshal parses the Message data and stores the result
-// in the value pointed to by v
+// Codec is the interface that wraps the nrpc Message data encoding method.
 type Codec interface {
 	Marshal(v interface{}) ([]byte, error)
 	Unmarshal(data []byte, v interface{}) error
 }
 
-// JSONCodec wraps std json
+// Wraps std json
 type JSONCodec struct{}
 
-// Marshal wraps std json.Marshal
+// Wraps std json.Marshal
 func (j *JSONCodec) Marshal(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
 
-// Unmarshal wraps std json.Unmarshal
+// Wraps std json.Unmarshal
 func (j *JSONCodec) Unmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
 
-// SetCodec sets default codec instance
+// Sets default codec instance
 func SetCodec(c Codec) {
 	DefaultCodec = c
 }
